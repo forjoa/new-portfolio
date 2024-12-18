a737fa187666e1a3113b539b9671dc7c1e968660
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-12-18 19:25:40 +0100

    refactor: receive classname props

--- src/components/globals/Main.tsx
+++ src/components/globals/Main.tsx
@@ -1,7 +1,16 @@
-export default function Main({ children }: { children: React.ReactNode }) {
-  return (
-    <main className='max-w-[800px] mx-auto bg-white rounded border backdrop-blur z-10 p-4 shadow-sm'>
-      {children}
-    </main>
-  )
-}
+export default function Main({
+    className,
+    children,
+  }: {
+    className?: string
+    children: React.ReactNode
+  }) {
+    return (
+      <main
+        className={`${className} max-w-[800px] mx-auto bg-white rounded border backdrop-blur z-10 p-4 shadow-sm`}
+      >
+        {children}
+      </main>
+    )
+  }
+  
