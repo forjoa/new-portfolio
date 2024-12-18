7c4f23b8dd8057cef0d5484df9fcabfde9f747ae
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-12-18 20:47:07 +0100

    feat: implement marquee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,29 @@
-import { DockDemo } from '@/components/dock-main'
+import { DockMain } from '@/components/dock-main'
 import Main from '@/components/globals/Main'
-
+import { MarqueeLanguages } from '@/components/languages'
 import DotPattern from '@/components/ui/dot-pattern'
+import { cn } from '@/lib/utils'
 
 export type IconProps = React.HTMLAttributes<SVGElement>
 
 export default function Home() {
   return (
     <>
-      <DotPattern width={20} height={20} cr={0.8} />
+      <DotPattern
+        width={20}
+        height={20}
+        cr={0.8}
+        className={cn(
+          '[mask-image:radial-gradient(900px_circle_at_center,white,transparent)]'
+        )}
+      />
       <Main>
         <div className='relative'>
-          <DockDemo />
+          <DockMain />
         </div>
+      </Main>
+      <Main className='mt-8'>
+        <MarqueeLanguages />
       </Main>
     </>
   )
