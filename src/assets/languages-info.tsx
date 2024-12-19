86806920e00cf66690a38670a0f3429f24195a1c
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-12-19 08:23:23 +0100

    build: programming languages

---
+++ src/assets/languages-info.tsx
@@ -0,0 +1,64 @@
+import { Icons } from './icons'
+
+export const languages = {
+  frontend: [
+    {
+      name: 'HTML',
+      icon: Icons.frontend.html,
+    },
+    {
+      name: 'CSS',
+      icon: Icons.frontend.css,
+    },
+    {
+      name: 'Javascript',
+      icon: Icons.frontend.javascript,
+    },
+    {
+      name: 'Typescript',
+      icon: Icons.frontend.typescript,
+    },
+    {
+      name: 'React',
+      icon: Icons.frontend.react,
+    },
+    {
+      name: 'Tailwind',
+      icon: Icons.frontend.tailwind,
+    },
+  ],
+  backend: [
+    {
+      name: 'Java',
+      icon: Icons.backend.java,
+    },
+    {
+      name: 'PHP',
+      icon: Icons.backend.php,
+    },
+    {
+      name: 'Node.js',
+      icon: Icons.backend.node,
+    },
+    {
+      name: 'Next.js',
+      icon: Icons.backend.next,
+    },
+    {
+      name: 'Spring',
+      icon: Icons.backend.spring,
+    },
+    {
+      name: 'PostgreSQL',
+      icon: Icons.backend.postgre,
+    },
+    {
+      name: 'MySQL',
+      icon: Icons.backend.mysql,
+    },
+    {
+      name: 'Docker',
+      icon: Icons.backend.docker,
+    },
+  ],
+}
