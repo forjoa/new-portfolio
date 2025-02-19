afce119e02d493b192e4cdf1c577a1eee4d33f59
joaquin <joaquin_trujillo@icloud.com>
2025-02-19 20:18:54 +0100

    build: v1 filter content

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,65 @@
 import Main from '@/components/ui/main'
+import SelectFilter from '@/components/select-filter'
 
-export default function About() {
+const aboutContent = [
+  {
+    title: 'About me',
+    description:
+      'I am a software developer with a focus on web development. I am passionate about building things that are useful and easy to use.',
+    category: 'personal',
+  },
+  {
+    title: 'Experience',
+    description:
+      'I have experience in web development, mobile development, and game development.',
+    category: 'professional',
+  },
+  {
+    title: 'Education',
+    description:
+      'I am a student of the Computer Science degree at the Universidad de Costa Rica.',
+    category: 'professional',
+  },
+  {
+    title: 'Skills',
+    description:
+      'I am proficient in JavaScript, TypeScript, React, Next.js, and Tailwind CSS.',
+    category: 'professional',
+  },
+  {
+    title: 'Interests',
+    description:
+      'I am interested in web development, mobile development, and game development.',
+    category: 'personal',
+  }
+]
+
+export default function About({ searchParams }: { searchParams: { filter?: string } }) {
+  const filter = searchParams?.filter || 'all'
+
   return (
     <>
       <Main>
-        <p>About me component</p>
+        <p>Filter content</p>
+        <p>param: {filter}</p>
+
+        <SelectFilter defaultValue={filter} />
+
+        {filter !== 'all'
+          ? aboutContent
+            .filter(c => c.category === filter)
+            .map((item, index) => (
+              <div key={index} className='mb-4'>
+                <p>{item.title}</p>
+                <p>{item.description}</p>
+              </div>
+            ))
+          : aboutContent.map((item, index) => (
+            <div key={index} className='mb-4'>
+              <p>{item.title}</p>
+              <p>{item.description}</p>
+            </div>
+          ))}
       </Main>
     </>
   )
